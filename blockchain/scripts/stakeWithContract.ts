c704da86e264cf56efdc9e2c0d5cd621abcbc91f
Morgan Weaver <morganjweaver@gmail.com>
2022-08-27 21:18:57 +0100

    cleaned up staking script

--- blockchain/scripts/stakeWithContract.ts
+++ blockchain/scripts/stakeWithContract.ts
@@ -1,6 +1,6 @@
 import { Contract, ethers } from "ethers"; // Hardhat for testing
 import "dotenv/config";
-import * as Staking from "../artifacts/contracts/Staking.sol/Staking.json";
+import * as StakingJSON from "../artifacts/contracts/Staking.sol/Staking.json";
 import { Staking } from "../typechain-types/contracts/Staking";
 import {
   getSigner,
@@ -14,19 +14,22 @@ async function main() {
   if (!checkBalance(signer)) {
     return;
   }
-  if (process.argv.length < 3) throw new Error("Please input staking contract address");
+  if (process.argv.length < 4) throw new Error("Please input staking contract address and deposit amount (don't forget leading zero as in 0.2)");
   const stakingAddress = process.argv[2];
+  const depositAmount = process.argv[3].toString();
+
   console.log(`Attaching token contract interface to address ${stakingAddress}`);
-  const stakingContract = new Contract(
+  const stakingContract: Staking = new Contract(
     stakingAddress,
-    Staking.abi,
+    StakingJSON.abi,
     signer
   ) as Staking;
+
+  const tx = await stakingContract.stake({value: ethers.utils.parseEther(depositAmount)})
   console.log("Awaiting confirmations");
-  await StakingContract.deployed();
-  console.log("Completed");
-  console.log(`SFT contract deployed at ${StakingContract.address}`);
-  console.log(`REMINDER: Please send a small amount of ETH to the contract for staking returns.`)
+  await tx.wait();
+  console.log(`Transaction completed. Hash: ${tx.hash}`);
+  console.log(`Deposit tx completed at ${stakingContract.address}`);
 }
 
 main().catch((error) => {
