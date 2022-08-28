8e04dc09b9f98c0e9411e115f96b631f8e769329
Hrvoje <stjepan.hrvoje@gmail.com>
2022-08-28 12:00:28 +0200

    Initial tests setup with chainlink price feed and VRF mock

--- blockchain/tests/LiquiBet.ts
+++ blockchain/tests/LiquiBet.ts
@@ -3,6 +3,7 @@ import { ethers } from "hardhat";
 // eslint-disable-next-line node/no-missing-import
 import { SFT } from "../typechain-types/contracts";
 import { Liquibet } from "../typechain-types/contracts/LiquiBet.sol";
+import { AggregatorV3Interface } from "../typechain-types"
 // eslint-disable-next-line node/no-unpublished-import
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 // eslint-disable-next-line node/no-unpublished-import
@@ -11,6 +12,9 @@ import { BigNumber } from "ethers";
 const TOKEN_UPDATE_INTERVAL = 100;
 const LIQUIBET_CONTRACT_FEE = 10000;
 
+const ASSET_DECIMALS: string = `18`;
+const ASSET_INITIAL_PRICE: string = `200000000000000000000`;
+
 describe("Liquibet contract", async () => {
   let tokenContract: SFT;
   let liquiBetContract: Liquibet;
@@ -18,14 +22,20 @@ describe("Liquibet contract", async () => {
   
   beforeEach(async () => {
     accounts = await ethers.getSigners();
-    const [tokenContractFactory, liquiBetContractFactory] =
-      await Promise.all([
-        ethers.getContractFactory("SFT"),
-        ethers.getContractFactory("Liquibet"),
-      ]);
+
+    const tokenContractFactory = await ethers.getContractFactory("SFT"); 
+    const liquiBetContractFactory = await ethers.getContractFactory("Liquibet");
+    const aggregatorContractFactory = await ethers.getContractFactory("MockV3Aggregator")
+    
+    const aggregatorContract = await aggregatorContractFactory.deploy(
+      ASSET_DECIMALS,
+      ASSET_INITIAL_PRICE
+    )
+    await aggregatorContract.deployed();
+
     tokenContract = await tokenContractFactory.deploy(
       TOKEN_UPDATE_INTERVAL,
-      accounts[1].address
+      aggregatorContract.address
     ) as SFT;
     await tokenContract.deployed();
 
@@ -37,23 +47,25 @@ describe("Liquibet contract", async () => {
   });
   
   describe("When the liquibet contract is deployed", async function() {
-    it("defines the fee amount", async function() {
-      const purchaseRatio = Number(ethers.utils.formatEther(await liquiBetContract.fee()));
-      expect(purchaseRatio).to.eq(LIQUIBET_CONTRACT_FEE);
+    it("should define the fee amount", async function() {
+      const fee = Number(ethers.utils.formatEther(await liquiBetContract.fee()));
+      expect(fee).to.eq(LIQUIBET_CONTRACT_FEE);
     });
 
-    it("uses valid ERC1155 token", async function() {
-      const liquiBetContractAddress = await liquiBetContract.token();
+    it("should use valid ERC1155 token", async function() {
+      const liquiBetTokenContract = await liquiBetContract.token();
       const tokenContractFactory = await ethers.getContractFactory("SFT");
-      const tokenContract = await tokenContractFactory.attach(liquiBetContractAddress);
+      const tokenContract = await tokenContractFactory.attach(liquiBetTokenContract);
 
-      const [tier1, initialPrice] = await Promise.all([
+      const [tier1, tier5, initialPrice] = await Promise.all([
         tokenContract.TIER_1(),
+        tokenContract.TIER_5(),
         tokenContract.initialPrice()
       ]);
 
-      expect(tier1.length).to.greaterThan(0);
-      expect(initialPrice.length).to.greaterThan(0);
+      expect(tier1).to.eq(1);
+      expect(tier5).to.eq(5);
+      expect(initialPrice).to.eq(ASSET_INITIAL_PRICE);
     });
   })
 });
