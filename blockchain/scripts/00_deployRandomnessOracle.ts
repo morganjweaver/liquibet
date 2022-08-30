c633d93435b4a28e7f5544c6f5d05ac4ecc1312e
Hrvoje <stjepan.hrvoje@gmail.com>
2022-08-30 20:37:22 +0200

    Liquibet contract deployment script fix

--- blockchain/scripts/00_deployRandomnessOracle.ts
+++ blockchain/scripts/00_deployRandomnessOracle.ts
@@ -13,9 +13,7 @@ async function main() {
   if (!checkBalance(signer)) {
     return;
   }
-  if (process.argv.length < 3) throw new Error("Fee (in wei) missing");
-  const fee = process.argv[2];
-  
+    
   // First deploy randomness oracle
   console.log("Deploying Randomness contract");
   const VRFFactory = new ethers.ContractFactory(
@@ -39,30 +37,6 @@ async function main() {
   ) as VRFv2Consumer;
   VRFConsumer.requestRandomWords(); //load randomness
   console.log("Requested random words for lottery");
-  
-  // Now deploy LiquiBet 
-  console.log("Deploying LiquiBet contract");
-  const LiquiBetFactory = new ethers.ContractFactory(
-    LiquiBetJSON.abi,
-    LiquiBetJSON.bytecode,
-    signer
-  );
-
-  const tokenUpdateInterval = 60 * 60 * 24;
-  const priceFeedAddress = "0xa39434a63a52e749f02807ae27335515ba4b07f7";
-  const LiquiBetContract = await LiquiBetFactory.deploy(
-    tokenUpdateInterval,
-    priceFeedAddress,
-    VRFContract.address,
-    fee
-  );
-
-  console.log("Awaiting confirmation on LiquiBet deployment");
-  await LiquiBetContract.deployed();
-  console.log("Completed LiquiBet deployment");
-  console.log(`LiquiBet contract deployed at ${LiquiBetContract.address}`);
-  
-  console.log(`Please register contract${LiquiBetContract.address} at https://keepers.chain.link/goerli as Custom Logic Upkeep`);
 }
 
 main().catch((error) => {
---
+++ blockchain/scripts/01_deployLiquiBet.ts
@@ -0,0 +1,48 @@
+import { ethers, Contract } from "ethers"; // Hardhat for testing
+import "dotenv/config";
+import * as LiquiBetJSON from "../artifacts/contracts/LiquiBet.sol/LiquiBet.json";
+import * as VRFJSON from "../artifacts/contracts/VRFOracle.sol/VRFv2Consumer.json";
+import { VRFv2Consumer } from "../typechain-types";
+import {
+  getSigner,
+  checkBalance,
+} from "../helpers/utils";
+
+async function main() {
+  const signer = getSigner();
+  if (!checkBalance(signer)) {
+    return;
+  }
+  if (process.argv.length < 4) throw new Error("Fee (in wei) or vrf contract address missing");
+  const fee = process.argv[2];
+  const vrfContractAddress = process.argv[3];
+    
+  // Now deploy LiquiBet 
+  console.log("Deploying LiquiBet contract");
+  const LiquiBetFactory = new ethers.ContractFactory(
+    LiquiBetJSON.abi,
+    LiquiBetJSON.bytecode,
+    signer
+  );
+
+  const tokenUpdateInterval = 60 * 60 * 24;
+  const priceFeedAddress = "0xa39434a63a52e749f02807ae27335515ba4b07f7";
+  const LiquiBetContract = await LiquiBetFactory.deploy(
+    tokenUpdateInterval,
+    priceFeedAddress,
+    vrfContractAddress,
+    fee
+  );
+
+  console.log("Awaiting confirmation on LiquiBet deployment");
+  await LiquiBetContract.deployed();
+  console.log("Completed LiquiBet deployment");
+  console.log(`LiquiBet contract deployed at ${LiquiBetContract.address}`);
+  
+  console.log(`Please register contract${LiquiBetContract.address} at https://keepers.chain.link/goerli as Custom Logic Upkeep`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
