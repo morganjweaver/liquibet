fefe9ec3d6a8cead36e43ebe4484c3df8ad2aae4
Hrvoje <stjepan.hrvoje@gmail.com>
2022-09-01 12:35:16 +0200

    MySfts pool info and link to details

--- frontend/src/components/NftListItem.jsx
+++ frontend/src/components/NftListItem.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function NftListItem({ image, tier, amount }) {
+function NftListItem({ image, tier, amount, tokenId }) {
   // const file = item.item.metadata;
   return (
-    <div className="justify-center items-center mx-2 mb-5 border-2 border-pink-200 rounded">
+    <div className="justify-center items-center text-center mx-2 mb-5 border-2 border-pink-400 rounded">
       <div className="text-center">
         <p>Tier {tier}</p>
         <p>Amount: {amount}</p>
@@ -14,6 +15,9 @@ function NftListItem({ image, tier, amount }) {
         style={{ height: "320px" }}
         alt=""
       />
+      <div className="p-2">
+        <Link to={`/sft/${tokenId}`}>View details</Link>
+      </div>
     </div>
   );
 }
--- frontend/src/components/pages/MySFTs.jsx
+++ frontend/src/components/pages/MySFTs.jsx
@@ -33,17 +33,15 @@ function MySFTs() {
   if (!dataFetched) return <LoadingComponent />;
 
   return (
-    <div className="px-16 bg-primary h-screen">
-      <h1 className="text-4xl text-white">My SFTs</h1>
-      <h2 className="text-white mt-8">
-        This page shows the amount of all SFTs of user {data.owner}.
-      </h2>
-      <div className="mt-4 px-2 grid grid-flow-row grid-cols-5 grid-rows-2 justify-center items-center pt-5 text-white">
+    <div className="px-16 mt-4 bg-primary font-1 text-white">
+    <h1>POOL {poolId}</h1>
+      <div className="grid grid-flow-row grid-cols-5 grid-rows-2 justify-center items-center pt-5">
         {data.sfts.map((sft, id) => (
           <NftListItem
-            key={id}
+            key={id}  
             image={sft.imgSrc}
-            tier={sft.tokenId}
+            tier={sft.tierId}
+            tokenId={sft.tokenId}
             amount={utils.formatUnits(data.sfts[id].amount, 0)}
           />
         ))}
