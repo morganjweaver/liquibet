f26dbeaf609d7241222d06676870e718f6ba9a3f
Morgan Weaver <morganjweaver@gmail.com>
2022-09-13 18:01:31 +0200

    address upfate for frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 
 export const environment = {
   network: 'goerli',
-  liquibetContractAddress: '0xE157e2A65D2f53Ebf2d9d70449094d027C82Ec46',
+  liquibetContractAddress: '0x4Da9a571879cA856AF00b38E27fCde7862c5D948',
   production: false
 };
